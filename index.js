b4cbc13588ef35c14c5733f239f5fe33c0a58329
Araya4 <araya.wut@gmail.com>
2024-08-26 17:15:15 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const dictIframe = {'1':"k67GBsH6KHMt0sBlZam",'2':'k4vik4QaAhibmHBlZio','3':'k38EjQ3yvi40m6BlZim','4':'k2nISFY0nl4OY6BlZik',
+    '5':'kZyrYnOwlYFoiNBlZig','6':'k6SMmcKlfLs5H6BlZii','7':'k1zMQmiZCHqCMBBlZEi','8':'k7q3j3t4Vvbs0WBlZEg',
+    '9':'ko9txT3mPXqW44BlZEa','10':'k5roxIbt2Tn5YRBlZEc'
+}
+
+function changeSrc(id){
+    document.getElementById('myVideo').src = "https://www.dailymotion.com/embed/video/"+dictIframe[id];
+}
